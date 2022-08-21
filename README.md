25747bcd38ad6babd07a4f3edaa322751d87f158
Mathias <mathias.schmidt@hey.com>
2022-08-21 18:36:34 +0200

    Fill readme, sort scripts

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # projektwoche-valorant-api
-A project to learn API calls and scripting
+
+This is a script collection to learn basic scripting.
+
+Contents are:
+- print
+- dictionaries
+- formatting
+- GET requests
+- data processing
+
+To start, clone this repo and start at [Lesson 1. Hello World](lessons/1.%20hello%20world.py)
+
+## Prerequisites
+
+- Install [Python](https://www.python.org/downloads/)
+- Install [Visual Studio Code](https://code.visualstudio.com/)
+- Within Visual Studio Code install the Python extension (Extension Id: ms-python.python )
---
+++ lessons/4. response.py
@@ -0,0 +1,23 @@
+# Ignore the following code. Start at line 16
+
+import requests
+import json
+import pprint
+
+pp = pprint.PrettyPrinter(sort_dicts=False)
+
+url = "https://valorant-api.com/v1/agents/dade69b4-4f5a-8528-247b-219e5a1facd6"
+
+response = requests.request("GET", url)
+
+response = json.loads(response.text)
+data = response["data"]
+
+# We have data from a valorant API stored in a variable called 'data'
+# TODO: Let's have a look into it, print 'data'
+
+
+# TODO: Use the pretty print command 'pp.pprint()' to see a better readable output
+
+
+# TODO: Print the value of the key 'displayName' from the dictionary data
